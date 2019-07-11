53e183bec27a50daaf9c30108bc6e6abf4a7b2e2
unknown <kell8888bell@gmail.com>
2019-07-11 10:18:39 -0400

    cleaned up the tests, added a few tests, added info about the tests to the readme

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
   * Selenium WebDriver with Chromedriver
   * pytest
   * Python SimpleHTTPServer
-  * Jenkins
+  * Jenkins with Shining Panda
 
 ## And I found some bugs! :)
   There is a rounding issue in the underlying code...the code used for the formatter is not great, but it does the job, for the most part. Should totally be refactored, but I am going for speed here...
@@ -27,3 +27,18 @@
   * 555.555 incorrectly rounds to 555.55, however 0.555 rounds to 0.56
   * -0.00001, returns -0.00 when all the other zero based tests return 0.00
   * 500.005 should round to 500.01, but the code returns 500.00
+
+## Reasons for some of my choices
+
+I didn't use parameterized tests because, in this case, they are too slow. Running as a single test it kicks off one browser window and throws 8 values in the while loop, clearing each time, it takes 10 seconds. Running as a parametrized test - which means it closes and opens the browser every time - it takes 60 seconds for the same 8 test values. Speed is important, feedback has to be fast, and there is no positive tradeoff for it to be so slow. Speed is also the reason I don't run headless...it's several seconds slower to run all the tests in the file headless. Sometimes things that you think will help can actually slow it down. As the automation lead at Veracode, I found the number of parallel sessions we could use when running in Jenkins was variable, hitting a multi-tiered stack at a busy time could handle fewer sessions than a single tiered stack that was only running my tests, it would show false failures because they were getting blocked at the server end when everyone was trying to run complex automation at the same time (limits of our test environment, not the product).
+
+I find that markers clutter the view - that big line blocks my view of the def. I use them when needed, when tests need to be grouped across files of tests, etc. but I usually wouldn't use them in a single file situation. We started using them a lot more when we were running in Jenkins because it was easier to group tests into suites, and I put the pytest line into the job description so that we could tell at a glance which blocks were failing.
+
+Pytest will run anything that starts with "test_" so I just change the first part of the name to something else, usually xxxtest_, if I am just trying to isolate a test case - it's easier to add them in, move them around, etc. using "find" while I am working, instead of dealing with a decorator. I will use FAILtest_ if it is a bit more permanent, e.g. if I am just waiting for a fix to come in before it gets checked into the master suite and I can find it easily searching for FAIL when the fix comes in. I will use the skip marker when the parent class has a marker, so all tests inherit that marker, and I am not very hopeful that the broken test will ever be fixed, but I am not going to throw away the test - it serves as a reminder that we thought about testing whatever it is. I could comment out the test, but I used epydoc to get a HTML browsable/searchable list of all the tests and it throws all comments in there, so I didn't want "archived" tests in the doc.
+
+Testing the formatting function via the GUI is not a bad idea - that is where the formatting is happening. I created some basic GUI tests (check for text present, etc.) to check the GUI itself, but all the other tests are testing the formatting at the GUI end - using a GUI tool, but not testing the GUI, per se. It is a fully functioning app and meets the requirements: Add associated tests for the functionality and for the user interface. It's not really conducive to performance testing, but then how much performance test data are you going to get throwing Jmeter at a currency format function? It might get some info on the performance of moving that data to a database, but everything I am doing is local to my machine and my database table would be tiny, a single table with a few columns, the limits found would be with my hardware.
+There are always other ways to do things, of course...
+ * the data that is typed in to the front end could be passed to the back end for formatting and then passed back and displayed
+  * if this required a huge number cruncher and needed the power of a back end server, perhaps that would be the way to go, however, for something this small, the security code to sanitize all the data would add layers of code and a lot more testing.
+ * the data could be formatted and then passed to a database and the formatting could be tested at the DB end
+  * that would be points of failure...what if the function that moved the data changed it in some way?
--- pythontests.py
+++ pythontests.py
@@ -1,23 +1,19 @@
 import pytest
 from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
 class TestTestGUI():
   def setup_method(self, method):
+    #chrome_options = Options()
+    #chrome_options.add_argument("--disable-gpu")
+    #chrome_options.add_argument("--headless")
+    #self.browser = webdriver.Chrome(options=chrome_options)
     self.browser = webdriver.Chrome()
-    # self.options = webdriver.ChromeOptions()
-    # run headless
-    # self.options.add_argument('headless')
-    # set the window size
-    # self.options.add_argument('window-size=1200x600')
-    # initialize the driver
-    #self.browser = webdriver.Chrome(chrome_options=options)
     self.vars = {}
+    self.browser.get("http://localhost:1337/FormatMoney.html")
+    self.browser.set_window_size(1000, 725)
 
   def teardown_method(self, method):
     self.browser.quit()
@@ -29,8 +25,6 @@ class TestTestGUI():
     return self.browser.find_element(By.ID, "letsGo").text
 
   def test_testGUI(self):
-    self.browser.get("http://localhost:1337/FormatMoney.html")
-    self.browser.set_window_size(1000, 725)
     assert self.browser.find_element(By.CSS_SELECTOR, "h2").text == "Challenge for Pleo"
     assert self.browser.find_element(By.ID, "inputValue").get_attribute('placeholder') == "amount"
     assert self.browser.find_element(By.ID, "inputValue").text == ""
@@ -55,16 +49,12 @@ class TestTestGUI():
     # data provided by Challenge
     #    formatMoney(2310000.159897); // '2 310 000.16'
     #    formatMoney(1600); // '1 600.00'
-    self.browser.get("http://localhost:1337/FormatMoney.html")
-    self.browser.set_window_size(1000, 725)
     assert self.check_result("2310000.159897") == "Result: 2 310 000.16"
     self.browser.refresh();   # refresh to start clean
     assert self.check_result("1600") == "Result: 1 600.00"
 
-  def test_testValues(self):
-    self.browser.get("http://localhost:1337/FormatMoney.html")
-    self.browser.set_window_size(1000, 725)
-
+  # this while loop test take 10 seconds to run because it uses a single broswer window
+  def test_testValuesIntegers(self):
     # test negative/positive integers
     inputListInt = [111, 2222, 333333, 44444444, -555, -6666, -777777, -88888888]
     outputListInt = ["111.00", "2 222.00", "333 333.00", "44 444 444.00", \
@@ -76,7 +66,23 @@ class TestTestGUI():
         self.browser.refresh();
         i += 1
 
-    # test negative/positive floats - see FAILtest_testRound1 for bug test
+  # this parameterized test take 60 seconds to run
+  # it launches a fresh browser for every value
+  # it does make it easier to see the individual results
+  # with testdata being defined before, it's harder to read
+  # even though the test itself is only one line
+  testdata = [(111, "111.00"),(2222, "2 222.00"),
+              (333333, "333 333.00"),(44444444, "44 444 444.00"),
+              (-555, "-555.00"),(-6666, "-6 666.00"),
+              (-777777, "-777 777.00"),(-88888888, "-88 888 888.00")]
+  @pytest.mark.skip(reason="it's too slow")
+  @pytest.mark.parametrize("input, output", testdata)
+  def test_testValuesIntegersP(self, input, output):
+    # test negative/positive integers
+    assert self.check_result(input) == "Result: " + output
+
+  def test_testValuesFloats(self):
+    # test negative/positive floats
     inputListFlt = ["111.111", "2222.2222", "333333.333333", "44444444.44444444", \
                     "-6666.6666", "-777777.777777", "-88888888.88888888"]
     outputListFlt = ["111.11", "2 222.22", "333 333.33", "44 444 444.44", \
@@ -88,6 +94,7 @@ class TestTestGUI():
         self.browser.refresh();
         i += 1
 
+  def test_testValuesLeadingZero(self):
     # test positive/negative mixed number of decimal places, with and without leading 0
     inputListDecWith = ["0.111", "0.44444444", "0.1234567890123456789012345678901234567890", \
                         "-0.555", "-0.88888888", "-0.1234567890123456789012345678901234567890"]
@@ -103,6 +110,7 @@ class TestTestGUI():
         self.browser.refresh();
         i += 1
 
+  def test_testZeros(self):
     # test positive and negative zeroes with and without leading 0 - see FAILtest_testRound2 for bug test
     inputListZeros = [0, -0, "0.0", "-0.0", ".0", "-.0", "0.", "-0.", "0.00", "-0.00", \
                       "0.0000", "-0.0000", "0.00001", ".00", "-.00", ".0000", "-.0000", ".00001"]
@@ -113,6 +121,7 @@ class TestTestGUI():
         self.browser.refresh();
         i += 1
 
+  def test_testBigNumbers(self):
     # test large numbers - using e/e+ (exponent) numbers in and out
     inputListBig = [1234567890123456789012345678901234567890, -1234567890123456789012345678901234567890, \
                      "1234567890123456789012345678901234567890.1234567890123456789012345678901234567890", \
@@ -132,9 +141,6 @@ class TestTestGUI():
         i += 1
 
   def test_testRounding(self):
-    self.browser.get("http://localhost:1337/FormatMoney.html")
-    self.browser.set_window_size(1000, 725)
-
     # test rounding up and down at the edge cases - see FAILtest_testRound3 for bug test
     inputListRnd = ["100.1", "100.01", "100.011", "100.001", \
                     "400.4", "400.04", "400.044", "400.004", \
@@ -153,11 +159,7 @@ class TestTestGUI():
         self.browser.refresh();
         i += 1
 
-  def test_testJunk(self):
-     # junk testing...woohoo! now it's getting fun :)
-    self.browser.get("http://localhost:1337/FormatMoney.html")
-    self.browser.set_window_size(1000, 725)
-
+  def test_testJunkShouldFail(self):
     # test that stuff that is not a number returns that message
     inputListJunk = ["Kelley", "( ͡° ͜ʖ ͡°)", "show me the money", "admin", "test", \
                      "NULL", "null", "true", "false", "TRUE", "FALSE", \
@@ -173,6 +175,7 @@ class TestTestGUI():
         self.browser.refresh();
         i += 1
 
+  def test_testJunkShouldPass(self):
     # test that stuff that is technically a number is ok
     inputListJunk2 = ["0100", "-0100", "0100.99", "0xabad1dea", "0x0", "+1", "+1.0"]
     outputListJunk2 = ["100.00", "-100.00", "100.99", "2 880 249 322.00", "0.00", "1.00", "1.00"]
@@ -183,24 +186,36 @@ class TestTestGUI():
         self.browser.refresh();
         i += 1
 
+  @pytest.mark.skip(reason="run with intention - it takes about an hour")
+  def test_testHammer(self):
+    # hammer the field with data continiously
+    inputHammer = 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+    i = 0
+    while i < 5000:
+        assert self.check_result(inputHammer) == "Result: 8.888888888888888e+99"
+        self.browser.refresh();
+        i += 1
 
   ######################
   ### TESTS FOR BUGS ###
   ######################
 
-  def FAILtest_testRound1(self):
+  @pytest.mark.skip(reason="this is a bug that hasn't been fixed - .555 rounding")
+  def test_testRound1(self):
     # bug where 500.005 rounds to 500.00 and not 500.01
     self.browser.get("http://localhost:1337/FormatMoney.html")
     self.browser.set_window_size(1000, 725)
     assert self.check_result("555.555") == "Result: 555.56"
 
-  def FAILtest_testRound2(self):
+  @pytest.mark.skip(reason="this is a bug that hasn't been fixed - returns negative 0")
+  def test_testRound2(self):
     # bug where -0.00001 rounds to -0.00 and not 0.00
     self.browser.get("http://localhost:1337/FormatMoney.html")
     self.browser.set_window_size(1000, 725)
     assert self.check_result("-0.00001") == "Result: 0.00"
 
-  def FAILtest_testRound3(self):
+  @pytest.mark.skip(reason="this is a bug that hasn't been fixed - .005 rounding")
+  def test_testRound3(self):
     # bug where 500.005 rounds to 500.00 and not 500.01
     self.browser.get("http://localhost:1337/FormatMoney.html")
     self.browser.set_window_size(1000, 725)
